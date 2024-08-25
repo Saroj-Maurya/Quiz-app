1a11929ffd259dae76b110b5c82c945f987f01de
shaqeebshaikh123 <90406905+Shk123s@users.noreply.github.com>
2024-08-25 17:11:26 +0530

    Added functionality to signup and login page

--- assets/script.js
+++ assets/script.js
@@ -1,60 +1,82 @@
-function handleSignup() {
+function registration() {
 
-    let signupName = document.getElementById("name").value;
-    let signupEmail = document.getElementById("email").value;
-    let signupPassword = document.getElementById("password").value;
-    if (signupEmail == "" || signupPassword == "") {
-        alert("all field is requid!");
+    const signupName = document.getElementById("name").value;
+    const signupEmail = document.getElementById("email").value;
+    const signupPassword= document.getElementById("password").value;
+
+    const namePattern = /^[A-Za-z\s\-']+$/;
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    
+    if(!signupName){
+        alert("Please enter your Full Name");
+        return
+    }
+
+    if(!namePattern.test(signupName)){
+        alert("Please enter valid name");
+        return
+    }
+
+    if(!signupEmail){
+        alert("Please enter your email");
+        return
+    }
+
+    if(!emailPattern.test(signupEmail)){
+        alert("Please enter valid email");
+        return
+    }
+
+    if(!signupPassword){
+        alert("Please enter your password");
+        return
+    }
+
+    if(signupPassword.length < 8){
+        alert("Password length should be 8 character!");
+        return
+    }
+
+    // save datd at localstorage
+    const users = JSON.parse(localStorage.getItem("user")) || [];
+    const userExist = users.some(
+        (newUser) => newUser.email === email
+    );
+    if (userExist){
+        alert("User Already exist");
         return;
     }
 
-    let isEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!isEmail.test(signupEmail)) {
-        alert("invalid Email!")
-        return false;
-    }
-
-    if (signupPassword.length < 6) {
-        alert("password length should be 6 character!");
-        return;
-    }
-
-
-
-    let users = JSON.parse(localStorage.getItem('user')) || [];
-    if (isUserExits(users, signupEmail)) {
-        alert("user already exits!");
-        return;
-    }
-
-    let userCredential = {
-        name: signupName,
+    const newUser = {
+        name : signupName,
         email: signupEmail,
-        password: signupPassword,
-        isLoggedIn: false
-    }
+        password : signupPassword,
+        isLoggedIn : false
+    };
 
-    users.push(userCredential);
-    localStorage.setItem("user", JSON.stringify(users));
+    users.push(newUser);
+    let objectConvertToString = JSON.stringify(users);
+    localStorage.setItem("user", objectConvertToString);
 
-    window.location.href = "login.html";
+    alert("Registration successful");
+    window.location = "index.html";
+
 }
 
-function isUserExits(users, email) {
-
+function isUserExits(users, email){
+        
     let userExist = false;
-    for (let i = 0; i < users.length; i++) {
-        if (users[i].email === email) {
+    for (let i=0; i<users.length; i++){
+        if(users[i].email === email){
             userExist = true;
             break;
         }
     }
-
     return userExist;
+
 }
 
-function handleLogin() {
-
+function login(){
 
     let loginEmail = document.getElementById("email").value;
     let loginPassword = document.getElementById("password").value;
@@ -65,7 +87,7 @@ function handleLogin() {
     let users = JSON.parse(localStorage.getItem('user'));
 
     if (loginEmail == "" || loginPassword == "") {
-        alert("all field is requid!");
+        alert("all field is required!");
         return;
     }
     let isValid = false;
@@ -77,7 +99,7 @@ function handleLogin() {
 
     }
     if (isValid) {
-        window.location.href = "quizstart.html";
+        window.location.href = "quizz.html";
     }
     else {
         alert("invalid data!");
